fa0d9ddec2a1e84af0e31120bf3df5cba85dfc5c
Arghya Guha <96729154+Arghya003@users.noreply.github.com>
2023-06-26 16:47:24 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (99.72%) - LeetSync

--- powx-n/powx-n.java
+++ powx-n/powx-n.java
@@ -1,20 +1,20 @@
 class Solution {
     public double myPow(double x, int n) {
-       double ans=1.0;
-        Long power = new Long(n);
-        if(power<0){
-            x=1/x;
-            power = Math.abs(power);
-        }
-        while(power>0){
-            if((power & 1)==1){
-                ans *= x;
-            }
-            x *= x;
-            power = power >> 1;
-        }
-        return ans;
-                
+      double ans = 1.0;
+    long nn = n;
+    if (nn < 0) nn = -1 * nn;
+    while (nn > 0) {
+      if (nn % 2 == 1) {
+        ans = ans * x;
+        nn = nn - 1;
+      } else {
+        x = x * x;
+        nn = nn / 2;
+      }
+    }
+    if (n < 0) ans = (double)(1.0) / (double)(ans);
+    return ans;
+  
         
     }
 }

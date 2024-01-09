b50640282bd7845aef7202b95a1adb47d4ea0200
Arghya Guha <96729154+Arghya003@users.noreply.github.com>
2024-01-09 12:33:30 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (22.60%) - LeetHub

--- 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.java
+++ 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.java
@@ -14,25 +14,26 @@
  * }
  */
 class Solution {
-    int min = Integer.MAX_VALUE;
-    int secondMin = Integer.MAX_VALUE;
-    boolean found = false;
-    public int findSecondMinimumValue(TreeNode root) {
-        if(root == null) return 0;
+    int min =Integer.MAX_VALUE;
+        int smin=Integer.MAX_VALUE;
+        boolean found=false;
+    public int findSecondMinimumValue(TreeNode root){
+       
         
-        if(root.val < min) {
-            min = root.val;
-        }
-        
-        if(root.val > min && root.val<=secondMin) {
-            found = true;
-            secondMin = root.val;
-        }
-        
-        findSecondMinimumValue(root.left);
-        findSecondMinimumValue(root.right);
-        
-        return !found ? -1 : secondMin ;
+        if(root==null)
+            return 0;
+        if(min>root.val)
+            min=root.val;
+    
+    if(root.val>min && smin>=root.val){
+        smin=root.val;
+        found=true;
         
     }
+    findSecondMinimumValue(root.left);
+    findSecondMinimumValue(root.right);
+    
+    return !found?-1:smin;
+}
+        
 }

c683cf10c12db2c337b19c2dd008f7e8e7067ef1
Arghya Guha <96729154+Arghya003@users.noreply.github.com>
2024-02-14 21:02:57 +0530

    Time: 16 ms (71.73%), Space: 45.3 MB (74.89%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,38 +1,50 @@
 class Solution {
-    private int dir[][] = new int[][]{{0,1},{0,-1},{1,0},{-1,0},{1,-1},{-1,1},{-1,-1},{1,1}};
+    
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-
-        if(grid[0][0]==1 || grid[m-1][n-1]==1) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
             return -1;
         }
 
-        boolean[][] visited = new boolean[m][n];
-        visited[0][0] = true;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0,0});
+        int ans = 0;
 
-        int ans=0;
+        int row = grid.length;
+        int col = grid[0].length;
+
+        if (grid[0][0] == 1 || grid[row - 1][col - 1] == 1) {
+            return -1;
+        }
+
+        int[][] dirs = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+
+        boolean[][] visited = new boolean[row][col];
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0, 0});
+        visited[0][0] = true;
+
         while (!queue.isEmpty()) {
             int size = queue.size();
-            for(int i=0;i<size;i++) {
-                int[] pop = queue.remove();
-                if(pop[0]==m-1 && pop[1]==n-1) {
-                    return ans+1;
-                }
-                for (int k=0;k<8;k++) {
-                    int nextX = dir[k][0]+pop[0];
-                    int nextY = dir[k][1]+pop[1];
+            ans++;
 
-                    if(nextX>=0 && nextX<m && nextY>=0 && nextY<n && !visited[nextX][nextY] && grid[nextX][nextY]==0) {
-                        queue.add(new int[]{nextX,nextY});
-                        visited[nextX][nextY]=true;
+            for (int i = 0; i < size; i++) {
+                int[] curPos = queue.poll();
+
+                if (curPos[0] == row - 1 && curPos[1] == col - 1) {
+                    return ans;
+                }
+
+                for (int[] dir : dirs) {
+                    int nextX = curPos[0] + dir[0];
+                    int nextY = curPos[1] + dir[1];
+
+                    if (nextX < 0 || nextX >= row || nextY < 0 || nextY >= col || visited[nextX][nextY] || grid[nextX][nextY] == 1) {
+                        continue;
                     }
 
+                    visited[nextX][nextY] = true;
+                    queue.offer(new int[]{nextX, nextY});
                 }
             }
-            ans++;
         }
 
         return -1;

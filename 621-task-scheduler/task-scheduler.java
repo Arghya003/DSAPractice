3e976e7022f27600cb93d7b34d6caa7fe453eec0
Arghya Guha <96729154+Arghya003@users.noreply.github.com>
2024-03-19 21:56:17 +0530

    Time: 2 ms (99.12%) | Memory: 46.1 MB (20.79%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,38 +1,18 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        // Build frequency map
-        int[] freq = new int[26];
-        for (char ch : tasks) {
-            freq[ch - 'A']++;
+       int[] freq = new int[26];
+        for(char ch: tasks){
+            freq[ch-'A']++;
         }
-        
-        // Max heap to store frequencies
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] > 0) {
-                pq.offer(freq[i]);
+        Arrays.sort(freq);
+        int max = freq[25];
+        int maxCount = 0; 
+        for(int i = 0; i<26; i++){
+            if(freq[i] == max){
+                maxCount++;  
             }
         }
 
-        int time = 0;
-        // Process tasks until the heap is empty
-        while (!pq.isEmpty()) {
-            int cycle = n + 1;
-            List<Integer> store = new ArrayList<>();
-            int taskCount = 0;
-            // Execute tasks in each cycle
-            while (cycle-- > 0 && !pq.isEmpty()) {
-                int currentFreq = pq.poll();
-                if (currentFreq > 1) {
-                    store.add(currentFreq - 1);
-                }
-                taskCount++;
-            }
-            // Restore updated frequencies to the heap
-            store.forEach(pq::offer);
-            // Add time for the completed cycle
-            time += (pq.isEmpty() ? taskCount : n + 1);
-        }
-        return time;
+        return Math.max(((max-1) * (n+1) + maxCount), tasks.length);
     }
 }
